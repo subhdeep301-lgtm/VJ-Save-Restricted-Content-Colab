ed42589edfff21ab052bb35aa673637b10e78e7d
subhdeep301-lgtm <subhdeep301@gmail.com>
2025-12-14 19:26:16 +0530

    Update bot.py restored

--- bot.py
+++ bot.py
@@ -1,21 +1,14 @@
-# # Don't Remove Credit Tg - @VJ_Botz
-# # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# # Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
-# from main import Bot
-# import TechVJ.speedtest
+from main import Bot
+import TechVJ.speedtest
 
-# Bot().run()
+Bot().run()
 
-# # Don't Remove Credit Tg - @VJ_Botz
-# # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# # Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 
-# bot.py
-# ENTRY FILE ONLY
-
-from main import main
-
-if __name__ == "__main__":
-    main()
